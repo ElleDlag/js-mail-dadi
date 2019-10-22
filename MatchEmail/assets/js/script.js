0d1f43dfa6748354b1085c8f693b9ecdbac62836
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-22 18:15:40 +0200

    fix cycle for

--- MatchEmail/assets/js/script.js
+++ MatchEmail/assets/js/script.js
@@ -1,33 +1,37 @@
 var dom = document;
-var listMail = dom.querySelectorAll('.item-mail h6');
+var listMail;
 var checkMail = dom.querySelector('#email');
 var checkBtn = dom.querySelector('.check-btn');
 var addBtn = dom.querySelector('.add-btn');
-var result = dom.querySelector('.result');
+var output = dom.querySelector('.result');
 var mailListGroup = dom.querySelector('.mail-list-group');
 var mailValue;
 var mailLenght;
+var n;
 
 checkBtn.addEventListener('click', function(){
+    listMail = dom.querySelectorAll('.item-mail h6');
     mailValue = checkMail.value;
     mailLength = mailValue.length;
-    alert(mailValue)
+    n = mailLength;
     if(!mailValue.includes('@') && !mailValue.includes('.') || mailLength <= 4){
         alert('Questa non sembra essere una mail')
     }else{
         for (var i = 0; i < listMail.length; i++) {
-            console.log(listMail[i].textContent)
             if(mailValue === listMail[i].textContent){
-                result.textContent = "Mail " + mailValue +" già presente nel database"
+                output.textContent = "Mail " + mailValue +" già presente nel database"
                 break;
-            }else{
-                if(confirm('Vuoi aggiungere la tua mail?')){
-                   mailListGroup.innerHTML += "<li class='list-group-item d-flex justify-content-between lh-condensed'>\
-                   <div class='item-mail'>\
-                   <h6 class='my-0'>" + mailValue + "</h6></div></li>";
+            } else{
+                if( i == ((listMail.length)-1)){
+                    if(confirm('Mail non presente. Vuoi aggiungere la tua mail?')){
+                        mailListGroup.innerHTML += "<li class='list-group-item d-flex justify-content-between lh-condensed'>\
+                        <div class='item-mail'>\
+                        <h6 class='my-0'>" + mailValue + "</h6></div></li>";
+                     }
+                     break;
                 }
-                break;
             }
+            checkMail.value = ""
         }
     }
 
