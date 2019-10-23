36fecd1b5cadf23b08dfc5b7f84ba3e626a98a22
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-23 16:01:56 +0200

    rebuild ex MatchMail

--- MatchEmail/assets/js/script.js
+++ MatchEmail/assets/js/script.js
@@ -7,9 +7,37 @@ var output = dom.querySelector('.result');
 var mailListGroup = dom.querySelector('.mail-list-group');
 var mailValue;
 var mailLenght;
-var n;
+var controller;
+/* var n; */
 
+//with "for" cycle without break
 checkBtn.addEventListener('click', function(){
+
+    listMail = dom.querySelectorAll('.item-mail h6');
+    mailValue = checkMail.value;
+    mailLength = mailValue.length;
+    n = mailLength;
+
+    //il controller non è perfetto servirebbe una funzione che matcha con una regular expression - da provare in BONUS
+    if(!mailValue.includes('@') && !mailValue.includes('.') || mailLength <= 4){
+        alert('Questa non sembra essere una mail')
+    } else{
+        for (var i = 0; i < listMail.length; i++) {
+            console.log(listMail[i].textContent)
+            if(mailValue === listMail[i].textContent ){
+                controller = true;
+                i = listMail.length+1;
+            } else {controller = false}       
+        }
+        if(controller == true){
+            alert("sei nella lista")
+        } else{ alert("non esiste")}
+    }
+});
+
+
+//with break version BAD IDEA :(
+/* checkBtn.addEventListener('click', function(){
     listMail = dom.querySelectorAll('.item-mail h6');
     mailValue = checkMail.value;
     mailLength = mailValue.length;
@@ -34,5 +62,5 @@ checkBtn.addEventListener('click', function(){
             checkMail.value = ""
         }
     }
+}) */
 
-})
